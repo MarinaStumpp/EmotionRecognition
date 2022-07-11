4a5a34668da6cae0024ed816b0155f9b3099c1c6
Marina Stumpp <et17048@lehre.dhbw-stuttgart.de>
2022-07-11 20:05:40 +0200

    added process audio

--- speech_prediction.py
+++ speech_prediction.py
@@ -1,5 +1,5 @@
 import argparse
-from speech_emotion_recognition_model import SpeechEmotion
+from speech_emotion_recognition import SpeechEmotion
 def parse_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
@@ -7,12 +7,13 @@ def parse_args():
     parser.add_argument('--speech_data_available', type=str, default=False) # True or false, is speech feature data available
     parser.add_argument('--speech_feature_type', type=str, default='mfcc') # mfcc or all
     parser.add_argument('--speech_model_type', type=str, default='our_model') # original, original_lstm, original_cnn, our_model
-    parser.add_argument('--speech_epochs', type=str, default=100) # number of training epochs
+    parser.add_argument('--speech_epochs', type=str, default=10) # number of training epochs
     parser.add_argument('--speech_runs', type=str, default=1) # number of runs to collect data for plotting
     parser.add_argument('--speech_ravdess_path', type=str, default='ravdess-emotional-speech-audio') # path to data
     parser.add_argument('--speech_audio_pickle_path', type=str, default='plotting_data/audio.dat') # path where plotting data is saved
     parser.add_argument('--speech_isConfusion', type=str, default='True') # Output of one confusion matrix
-
+    parser.add_argument('--video_path', type=str, default='ravdess-emotional-speech-audio/Actor_01/02-01-08-02-02-02-01.mp4') # Output of one confusion matrix
+    parser.add_argument('--audio_path', type=str, default='audio') # Output of one confusion matrix
     args = parser.parse_args()
     dict_args = vars(args)
 
@@ -24,3 +25,4 @@ if __name__ == "__main__":
     args = parse_args()
     speech_model = SpeechEmotion(**args)
     speech_model(**args)
+    print(speech_model.process_audio(**args))
