c8c96de060a1b540d17b2c9829fd5ba1efaa6655
MarinaStumpp <57017223+MarinaStumpp@users.noreply.github.com>
2022-07-12 09:42:46 +0200

    Update speech_prediction.py

--- speech_prediction.py
+++ speech_prediction.py
@@ -7,7 +7,7 @@ def parse_args():
     parser.add_argument('--speech_data_available', type=str, default=False) # True or false, is speech feature data available
     parser.add_argument('--speech_feature_type', type=str, default='mfcc') # mfcc or all
     parser.add_argument('--speech_model_type', type=str, default='our_model') # original, original_lstm, original_cnn, our_model
-    parser.add_argument('--speech_epochs', type=str, default=10) # number of training epochs
+    parser.add_argument('--speech_epochs', type=str, default=100) # number of training epochs
     parser.add_argument('--speech_runs', type=str, default=1) # number of runs to collect data for plotting
     parser.add_argument('--speech_ravdess_path', type=str, default='ravdess-emotional-speech-audio') # path to data
     parser.add_argument('--speech_audio_pickle_path', type=str, default='plotting_data/audio.dat') # path where plotting data is saved
