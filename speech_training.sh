768f0961f198beae0c4ef0ddfdacf8664e772622
MarinaStumpp <57017223+MarinaStumpp@users.noreply.github.com>
2022-07-21 16:23:48 +0200

    Update speech_training.sh

--- speech_training.sh
+++ speech_training.sh
@@ -4,5 +4,5 @@ py -3 speech_training.py \
 --speech_data_available True \
 --speech_model_type 'our_model' \
 --speech_feature_type 'mfcc' \
---speech_epochs 10 \
+--speech_epochs 100 \
 --speech_runs 1
